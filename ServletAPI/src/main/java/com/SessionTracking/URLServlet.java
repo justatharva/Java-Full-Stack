8f718186938db389d4a1bd71515849542200a6a0
JustAtharva <atharvaking2080@gmail.com>
2026-08-11 12:32:56 +0530

    Some Changes Required

---
+++ ServletAPI/src/main/java/com/SessionTracking/URLServlet.java
@@ -0,0 +1,28 @@
+package com.SessionTracking;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/SessionTracking/URLServlet")
+public class URLServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("Welcome url rewriting form<br>");
+		
+		String name = request.getParameter("MyName");
+		String email = request.getParameter("MyEmail");
+		
+		out.println("Name is "+name+"<br>");
+		out.println("Email is "+email+"<br>");
+		
+		out.println("<a href='URLServlet2' MyName = ? MyEmail = ?>Click Me</a>");
+	}
+}
---
+++ ServletAPI/src/main/java/com/SessionTracking/URLServlet2.java
@@ -0,0 +1,28 @@
+package com.SessionTracking;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/SessionTracking/URLServlet2")
+public class URLServlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		out.println("Welcome Back URL Servlet<br>");
+		
+		String n = request.getParameter("MyName");
+		String e = request.getParameter("MyEmail");
+		
+		out.println("Name is "+n+"<br>");
+		out.println("Email is "+e+"<br>");
+		
+		
+	}
+}
